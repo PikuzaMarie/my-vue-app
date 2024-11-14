3be70a953e26d53ae8a6f396bfdc27e994df5db6
PikuzaMarie <pikuza.mari@gmail.com>
2024-11-14 14:52:18 +0300

    feat: create header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,4 +1,8 @@
 <template>
-  <v-toolbar><v-app-bar-nav-icon></v-app-bar-nav-icon></v-toolbar>
+  <v-toolbar class="d-flex justify-content-space-between pl-6 pr-6" color="primary">
+    <v-icon icon="mdi-menu" size="x-large" />
+    <v-spacer></v-spacer>
+    <v-icon icon="mdi-magnify" size="x-large" />
+  </v-toolbar>
 </template>
 <script></script>
