09055360fc1a4c41cb4cdd60689e8302f43af0d2
PikuzaMarie <pikuza.mari@gmail.com>
2024-11-14 14:52:05 +0300

    feat: add material icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const vuetify = createVuetify({
   theme: {
@@ -9,6 +10,13 @@ const vuetify = createVuetify({
         secondary: '#424242',
         accent: '#82B1FF',
       },
+    },
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
     },
   },
 })
