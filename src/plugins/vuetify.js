7eded4197d41b6d6611b5d89fc37a5787241c64f
PikuzaMarie <pikuza.mari@gmail.com>
2024-11-15 20:41:36 +0300

    style: change primary color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,7 +6,7 @@ const vuetify = createVuetify({
   theme: {
     themes: {
       light: {
-        primary: '#1976D2',
+        primary: '#0f4c82',
         secondary: '#424242',
         accent: '#82B1FF',
       },
