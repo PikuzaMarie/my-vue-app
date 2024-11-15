a1ba3abf6f8e53215b927114b1b10d65dc231fee
PikuzaMarie <pikuza.mari@gmail.com>
2024-11-15 21:01:04 +0300

    style: style filter and its components

--- src/components/FilterSidebar.vue
+++ src/components/FilterSidebar.vue
@@ -1,65 +1,69 @@
 <template>
   <v-navigation-drawer
     v-model="isFilterOpen"
-    width="350"
+    width="434"
     transition="transform 0.3s ease"
     permanent
     location="right"
-    class="d-flex ga-4 pt-8 pr-4 pl-4"
+    class="d-flex ga-4 pt-8 pr-8 pb-8 pl-8"
+    elevation="2"
   >
-    <v-form ref="filterForm">
-      <v-text-field
-        v-model="filters.firstName"
-        label="Имя"
-        @input="updateFilters"
-        density="compact"
-        variant="solo"
-      />
-      <v-text-field
-        v-model="filters.lastName"
-        label="Фамилия"
-        @input="updateFilters"
-        density="compact"
-        variant="solo"
-      />
-      <v-text-field
-        v-model="filters.company"
-        label="Компания"
-        @input="updateFilters"
-        density="compact"
-        variant="solo"
-      />
-      <v-text-field
-        v-model="filters.jobTitle"
-        label="Специальность"
-        @input="updateFilters"
-        density="compact"
-        variant="solo"
-      />
-      <v-text-field
-        v-model="filters.phone"
-        label="Телефон"
-        @input="updateFilters"
-        density="compact"
-        variant="solo"
-      />
-      <v-text-field
-        v-model="filters.email"
-        label="E-mail"
-        @input="updateFilters"
-        density="compact"
-        variant="solo"
-      />
-      <v-text-field
-        v-model="filters.interests"
-        label="Интересы"
-        @input="updateFilters"
-        density="compact"
-        variant="solo"
-      />
-      <v-card variant="text" class="d-flex justify-space-between mt-6">
-        <v-btn @click="resetFiltersFromLocal" class="mb-1">Сбросить</v-btn>
-        <v-btn color="primary" @click="applyFilters" class="mb-1">Применить</v-btn>
+    <v-form ref="filterForm" class="d-flex flex-md-column justify-space-between fill-height">
+      <v-card variant="text" class="d-flex flex-md-column ga-md-1">
+        <v-text-field
+          v-model="filters.firstName"
+          label="Имя"
+          @input="updateFilters"
+          density="compact"
+          variant="solo"
+        />
+        <v-text-field
+          v-model="filters.lastName"
+          label="Фамилия"
+          @input="updateFilters"
+          density="compact"
+          variant="solo"
+        />
+        <v-text-field
+          v-model="filters.company"
+          label="Компания"
+          @input="updateFilters"
+          density="compact"
+          variant="solo"
+        />
+        <v-text-field
+          v-model="filters.jobTitle"
+          label="Специальность"
+          @input="updateFilters"
+          density="compact"
+          variant="solo"
+        />
+        <v-text-field
+          v-model="filters.phone"
+          label="Телефон"
+          @input="updateFilters"
+          density="compact"
+          variant="solo"
+        />
+        <v-text-field
+          v-model="filters.email"
+          label="E-mail"
+          @input="updateFilters"
+          density="compact"
+          variant="solo"
+        />
+        <v-text-field
+          v-model="filters.interests"
+          label="Интересы"
+          @input="updateFilters"
+          density="compact"
+          variant="solo"
+        />
+      </v-card>
+
+      <v-card variant="text" class="d-flex justify-end mt-auto ga-md-5">
+        <v-btn @click="resetFiltersFromLocal" class="mb-1 text-body-2">Очистить</v-btn>
+        <v-btn color="primary" @click="applyFilters" class="mb-1 text-body-2">Применить</v-btn>
       </v-card>
     </v-form>
   </v-navigation-drawer>
